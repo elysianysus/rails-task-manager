fe72ba5b6333d7def61d0671212fb3eec9d20032
LEUNG KAM YING (vincci) <128721163+elysianysus@users.noreply.github.com>
2023-11-08 21:11:03 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: redirect('/tasks')
+  
   get "tasks", to: "tasks#index"
 
   get "tasks/new", to: "tasks#new"
